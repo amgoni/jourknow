068e0f50eed57cb9b01d5a56f580e3cfadf79d80
Amin Mohammed Goni <amgoni.ug@atbu.edu.ng>
2023-07-23 17:15:12 +0100

    fixed redirect

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React, { useContext } from "react";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import "./app.scss";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import Welcome from "./components/Welcome";
+import AuthContext from "./store/auth-context";
 
 const App = () => {
+  const authCtx = useContext(AuthContext);
   return (
     <Router>
       <div>
@@ -20,7 +22,8 @@ const App = () => {
           exact
           render={(props) => (
             <>
-              <Welcome />
+              {/* Redirect to "/home" if the user is logged in */}
+              {authCtx.isLoggedIn ? <Redirect to="/home" /> : <Welcome />}
             </>
           )}
         />
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useHistory } from "react-router-dom";
 import "./welcome.scss";
 import Authentication from "./Authentication";
 
 const Welcome = () => {
+  const history = useHistory();
+
+  const isLoggedIn = () => {
+    // Check if the user is logged in (use your preferred method)
+    // For example, you can check if there's a stored token or any other authentication state
+    const token = localStorage.getItem("token");
+    return token !== null;
+  };
+
+  // Redirect to "/home" if the user is already logged in
+  useEffect(() => {
+    if (isLoggedIn()) {
+      history.replace("/home");
+    }
+  }, [history]);
+
   return (
     <div className="welcome">
       <div className="welcome-message">
